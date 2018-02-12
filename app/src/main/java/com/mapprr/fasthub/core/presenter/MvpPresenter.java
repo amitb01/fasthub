c09a8c92455788678411a007c0d52e5584017a8c
Amit <amitbansal.dtu@gmail.com>
2018-02-12 17:39:12 +0530

    MVP setup

--- app/src/main/java/com/mapprr/fasthub/core/presenter/MvpPresenter.java
+++ app/src/main/java/com/mapprr/fasthub/core/presenter/MvpPresenter.java
@@ -1,5 +1,75 @@
 package com.mapprr.fasthub.core.presenter;
 
-public class MvpPresenter {
+import android.util.Log;
 
+import java.lang.ref.WeakReference;
+
+public abstract class MvpPresenter<V extends MvpPresenter.View> {
+
+    private WeakReference<V> viewWeakRef;
+    private boolean isViewForeground;
+
+    /**
+     * Attach the view to the presenter. The presenter holds a weak reference to the view.
+     * This is called from the relevant lifecycle method in the view.
+     */
+    public void attachView(V view) {
+        if (view == null) {
+            throw new IllegalArgumentException("Cannot attach a null view to the presenter");
+        }
+        this.viewWeakRef = new WeakReference<>(view);
+        Log.i("%s attachView()", this.getClass().getSimpleName());
+    }
+
+    /**
+     * Returns the view reference if attached, else null. Use the {@link #isViewAttached()} method
+     * before calling this.
+     */
+    protected V getView() {
+        return viewWeakRef != null ? viewWeakRef.get() : null;
+    }
+
+    /**
+     * Returns a boolean stating whether the view is still attached or not
+     */
+    protected boolean isViewAttached() {
+        return getView() != null;
+    }
+
+    /**
+     * This method is called when the view comes to the foreground.
+     */
+    public void onViewForeground() {
+        isViewForeground = true;
+        Log.i("%s onViewForeground()", this.getClass().getSimpleName());
+    }
+
+    /**
+     * This method is called when the view goes in the background / another view comes to the foreground.
+     * The view is still attached to the presenter.
+     */
+    public void onViewBackground() {
+        isViewForeground = false;
+        Log.i("%s onViewBackground()", this.getClass().getSimpleName());
+    }
+
+    protected boolean isViewForeground() {
+        return isViewForeground;
+    }
+
+    /**
+     * Detach the view from the presenter.
+     * This is called from the relevant lifecycle method in the view.
+     */
+    public void detachView() {
+        if (viewWeakRef != null) {
+            viewWeakRef.clear();
+            viewWeakRef = null;
+        }
+    }
+
+    public interface View {
+
+    }
 }
+
--- app/src/main/java/com/mapprr/fasthub/core/view/MvpActivity.java
+++ app/src/main/java/com/mapprr/fasthub/core/view/MvpActivity.java
@@ -1,15 +1,52 @@
 package com.mapprr.fasthub.core.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-import com.mapprr.fasthub.R;
+import com.mapprr.fasthub.core.presenter.MvpPresenter;
 
-public class MvpActivity extends AppCompatActivity {
+import butterknife.ButterKnife;
+
+public abstract class MvpActivity<P extends MvpPresenter> extends AppCompatActivity implements MvpPresenter.View {
+
+    private P presenter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mvp);
+        if (getLayoutResourceId() != -1) {
+            setContentView(getLayoutResourceId());
+            ButterKnife.bind(this);
+        }
+
+        presenter = createPresenter(savedInstanceState);
+        getPresenter().attachView(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getPresenter().onViewForeground();
+    }
+
+    @Override
+    protected void onStop() {
+        getPresenter().onViewBackground();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        getPresenter().detachView();
+        super.onDestroy();
+    }
+
+    public abstract int getLayoutResourceId();
+
+    public abstract P createPresenter(Bundle savedInstanceState);
+
+    public final P getPresenter() {
+        return presenter;
     }
 }
